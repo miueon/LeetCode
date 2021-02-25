b8a60d8341b1d0bb987e699353e075d49cf9c1e9
miueon <cruxbell@gamil.com>
2021-02-25 10:57:39 +0800

    remove .idea

---
+++ src/main/java/com/miueon/leetcode/editor/en/1081SmallestSubsequenceOfDistinctCharacters.kt
@@ -0,0 +1,42 @@
+  //Return the lexicographically smallest subsequence of s that contains all the d
+//istinct characters of s exactly once. 
+//
+// Note: This question is the same as 316: https://leetcode.com/problems/remove-
+//duplicate-letters/ 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: s = "bcabc"
+//Output: "abc"
+// 
+//
+// Example 2: 
+//
+// 
+//Input: s = "cbacdcbc"
+//Output: "acdb"
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length <= 1000 
+// s consists of lowercase English letters. 
+// 
+// Related Topics String Stack Greedy 
+// 👍 809 👎 108
+
+  package com.miueon.leetcode.editor.en
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    fun smallestSubsequence(s: String): String {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  
+ 
---
+++ src/main/java/com/miueon/leetcode/editor/en/316RemoveDuplicateLetters.kt
@@ -0,0 +1,43 @@
+  //Given a string s, remove duplicate letters so that every letter appears once a
+//nd only once. You must make sure your result is the smallest in lexicographical 
+//order among all possible results. 
+//
+// Note: This question is the same as 1081: https://leetcode.com/problems/smalle
+//st-subsequence-of-distinct-characters/ 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: s = "bcabc"
+//Output: "abc"
+// 
+//
+// Example 2: 
+//
+// 
+//Input: s = "cbacdcbc"
+//Output: "acdb"
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length <= 104 
+// s consists of lowercase English letters. 
+// 
+// Related Topics String Stack Greedy 
+// 👍 2234 👎 168
+
+  package com.miueon.leetcode.editor.en
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    fun removeDuplicateLetters(s: String): String {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  
+ 
